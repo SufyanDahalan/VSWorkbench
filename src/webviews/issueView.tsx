d7ad6c8956f1602a649c22141f721ee7a86f0f23
Sufyan Dahalan <sufyan.dahalan@gmail.com>
2022-04-19 01:46:48 +0200

    some more progress w IssueView

--- src/webviews/issueView.tsx
+++ src/webviews/issueView.tsx
@@ -5,41 +5,49 @@ import "./App.css";
 import { Api } from "../api";
 let api = Api.Instance;
 
-function IssueView(props:any) {
-    // `props.rest` is the rest of the URL after "/profile/"
-    let issue:any;
+// function IssueView(props:any) {
+//     // `props.rest` is the rest of the URL after "/profile/"
+//     let issue:any;
 
-    api.getProjectIssue(props.projectID, props.issueID).then((res)=>{
-        console.log('res.data')
-        console.log(res.data)
-        issue = res.data
-    })
-    return (
-      <div>
-          {props.id}
-        { issue && 
+//     api.getProjectIssue(props.projectID, props.issueID).then((res)=>{
+//         console.log('res.data')
+//         console.log(res.data)
+//         issue = res.data
+//     })
+//     return (
+//       <div>
+//           {props.id}
+//         { issue &&
+//         <h1>
+//         {issue.title}
+//         </h1>
 
-        
-        <h1>
-        {issue.title} 
-        </h1>
+//         }
+//       </div>
+//     );
+//   }
 
-        }
-      </div>
-    );
-  }
-  
-//   class IssueView extends React.Component<string,{}>{
-//     constructor(props: string){
-//         super(props)
-//     }
-//     render() {
-//         return (
-//             <div>
-//                 {this.props.id}
-//                 <h1> Some Issue!</h1>
-//             </div>
-//         )
-//     }
-// }
+class IssueView extends React.Component<any, {}> {
+	state = {
+		issue: {} as IIssue,
+	};
+	constructor(props: any) {
+		super(props);
+	}
+	componentDidMount() {
+		api.getProjectIssue(this.props.projectID, this.props.issueID).then((res) => {
+			console.log("res.data");
+			console.log(res.data);
+			this.setState({ issue: res.data });
+		});
+	}
+	render() {
+		return (
+			<div>
+				{this.props.id}
+				<h1>{this.state.issue.title}</h1>
+			</div>
+		);
+	}
+}
 export default IssueView;
