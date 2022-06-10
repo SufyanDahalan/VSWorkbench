5fefe728d0a5619080bbe1c68bf4229c8267dfef
Sufyan Dahalan <sufyan.dahalan@gmail.com>
2022-06-10 02:09:26 +0200

    Working towards better support for different gitlab instances

--- src/globals/functions.ts
+++ src/globals/functions.ts
@@ -6,7 +6,6 @@ import { Api } from "../api";
 import { newAuthentication } from "./event";
 const api = Api.Instance;
 
-
 export async function settings(globalState: vscode.ExtensionContext["globalState"]) {
 	// get personal auth token and gitlab instance
 	let gitlabInstance = "",
@@ -18,57 +17,64 @@ export async function settings(globalState: vscode.ExtensionContext["globalState
 	});
 	inputPersonalAuthToken.onDidAccept(async () => {
 		inputPersonalAuthToken.hide();
-        let res = await checkGitlabInstanceAndAuthToken(gitlabAuthToken, gitlabInstance);
-        
+		if (gitlabInstance.endsWith('/') && !gitlabInstance.endsWith('/api/')) {
+            gitlabInstance += "api/";
+		}
+		let res = await checkGitlabInstanceAndAuthToken(gitlabAuthToken, gitlabInstance);
+
 		if (res) {
-            globalState.update(GITLAB_INSTANCE_KEY, gitlabInstance);
-            globalState.update(AUTH_TOKEN_KEY, gitlabAuthToken);
-            // TODO update all views and apps via an eventbus event/notification
-            newAuthentication.fire();
-        } else if (!res){
-            Api.updateAuthToken(globalState.get(AUTH_TOKEN_KEY) as string);
-            Api.updateBaseURL(globalState.get(GITLAB_INSTANCE_KEY) as string);
+			globalState.update(GITLAB_INSTANCE_KEY, gitlabInstance);
+			globalState.update(AUTH_TOKEN_KEY, gitlabAuthToken);
+			// TODO update all views and apps via an eventbus event/notification
+			newAuthentication.fire();
+		} else if (!res) {
+			Api.updateAuthToken(globalState.get(AUTH_TOKEN_KEY) as string);
+			Api.updateBaseURL(globalState.get(GITLAB_INSTANCE_KEY) as string);
 			// optionally show some error message depending on whats wrong
 			inputGitlabInstance.show();
 		}
 	});
 
 	const inputGitlabInstance = vscode.window.createInputBox();
-	inputGitlabInstance.placeholder = "Please Enter Your Gitlab Instance";
+	inputGitlabInstance.placeholder = "Please Enter Your Gitlab Instance [e.g. https://gitlab.com]";
 	inputGitlabInstance.onDidChangeValue((tokenInput) => {
 		gitlabInstance = tokenInput;
 	});
 	inputGitlabInstance.onDidAccept(() => {
-        gitlabInstance = gitlabInstance.length ? gitlabInstance :  GitLab_SaaS_Base_URL
+		gitlabInstance = gitlabInstance.length ? gitlabInstance : GitLab_SaaS_Base_URL;
 		inputGitlabInstance.hide();
 		inputPersonalAuthToken.show();
 	});
 	inputGitlabInstance.show();
 }
 
-export async function checkGitlabInstanceAndAuthToken(token: string, baseurl: string, startup?: boolean /* globalState: vscode.ExtensionContext["globalState"] */) {
+export async function checkGitlabInstanceAndAuthToken(
+	token: string,
+	baseurl: string,
+	startup?: boolean /* globalState: vscode.ExtensionContext["globalState"] */
+) {
 	// console.log(`auth token from chechAuthToken: |${globalState.get(AUTH_TOKEN_KEY)}|`);
 	Api.updateAuthToken(token);
 	Api.updateBaseURL(baseurl);
-    try{
-        let res = await api.getUserInfo()//.then((res) => {
-            if (res.status.toString()[0] === "2") {
-                if(startup){
-                    vscode.commands.executeCommand("setContext", "VSWorkbench.authenticated", true);
-                }
-                return true;
-            } else {
-                if(startup){
-                    vscode.commands.executeCommand("setContext", "VSWorkbench.authenticated", false);
-                }
-                return false;
-            }
-    } catch {
-        if(startup){
-            vscode.commands.executeCommand("setContext", "VSWorkbench.authenticated", false);
-        }
-        return false;
-    }
+	try {
+		let res = await api.getUserInfo(); //.then((res) => {
+		if (res.status.toString()[0] === "2") {
+			if (startup) {
+				vscode.commands.executeCommand("setContext", "VSWorkbench.authenticated", true);
+			}
+			return true;
+		} else {
+			if (startup) {
+				vscode.commands.executeCommand("setContext", "VSWorkbench.authenticated", false);
+			}
+			return false;
+		}
+	} catch {
+		if (startup) {
+			vscode.commands.executeCommand("setContext", "VSWorkbench.authenticated", false);
+		}
+		return false;
+	}
 	// });
 }
 /**
